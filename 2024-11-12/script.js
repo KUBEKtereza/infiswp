6967229d1bb1f66735b755950828fdc68f2d707a
Tereza Kubek <KUB230105@spengergasse.at>
2024-11-19 14:45:19 +0100

    ....

---
+++ 2024-11-12/script.js
@@ -0,0 +1,15 @@
+const filePath = '../nmap-datenfiles/2024-11-05T13:12:00+01:00';
+try {
+    const dirEntries = await Deno.readDir(filelocation);
+    for await (const dirEntry of dirEntries) {
+        console.log(dirEntry.name);
+        const data = await Deno.readTextFile(filePath);
+        const lines = data.split('\n');
+        console.log(lines);
+    }
+    
+    
+    
+} catch (err) {
+    console.error('Error reading the file:', err);
+}
